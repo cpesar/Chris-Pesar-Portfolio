a28881e4606464e2c27c380a30e273e84b7c5924
cpesar <cpesar1@gmail.com>
2022-02-09 19:58:35 -0700

    added project dependencies

---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const projects = [
+  {
+    title: "PizzaHunt",
+    category: "Blogs",
+    description:
+      "An app that allows users to create edit and delete pizza recipes",
+    site: "https://pizza-hunting.herokuapp.com/",
+    repo: "https://github.com/cpesar/pizza-hut",
+  },
+
+  {
+    title: "ShopShop",
+    category: "Ecommerce",
+    description:
+      "A simple e commerce app where users can add items to their cart and checkout using Stripe",
+    site: "https://the-online-store.herokuapp.com/",
+    repo: "https://github.com/cpesar/the-online-store",
+  },
+
+  {
+    title: "DockNFlock",
+    category: "Ecommerce",
+    description:
+      "An app that allows users to search for parking and tickets in one conveienent place",
+    site: "https://dock-and-flock.herokuapp.com/",
+    repo: "https://github.com/cpesar/dock-n-flock",
+  },
+
+  {
+    title: "BestSellers",
+    category: "API",
+    description:
+      "A simple app that allows a user to search for a book within a selected category. The user can then checkout their item which will link them to Amazon",
+    site: "https://cpesar.github.io/the-best-sellers/",
+    repo: "https://github.com/cpesar/the-best-sellers",
+  },
+
+  {
+    title: "GoogleBooks",
+    category: "API",
+    description: "Simple book search app",
+    site: "https://the-book-store-2.herokuapp.com/",
+    repo: "https://github.com/cpesar/the-book-store-2",
+  },
+
+  {
+    title: "WeatherApp",
+    category: "API",
+    description:
+      "A user can search for weather by city state or zipcode. Uses the openweather api",
+    site: "https://cpesar.github.io/weather-dashboard-refactor/",
+    repo: "https://github.com/cpesar/weather-dashboard-refactor",
+  },
+
+  {
+    title: "DeepThoughts",
+    category: "Blogs",
+    description: "A thought blog",
+    site: "https://deep-thinker.herokuapp.com/",
+    repo: "https://github.com/cpesar/deep-thoughts",
+  },
+
+  {
+    title: "ZooAnimals",
+    category: "Blogs",
+    description: "A zoo animal blog that uses CRUD routes",
+    site: "https://zoanimals.herokuapp.com/",
+    repo: "https://github.com/cpesar/zookeeper",
+  },
+];
