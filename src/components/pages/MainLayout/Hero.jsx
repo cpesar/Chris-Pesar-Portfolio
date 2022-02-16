68061b2a5f1d4e9a21034a35e2f360aebe6862e0
cpesar <cpesar1@gmail.com>
2022-02-16 15:32:17 -0700

    layout for hero and about sections

--- src/components/pages/MainLayout/Hero.jsx
+++ src/components/pages/MainLayout/Hero.jsx
@@ -1,109 +1,74 @@
 import React, { useRef, useEffect } from "react";
 
-import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+import { init } from "ityped";
+
+import styled from "styled-components";
+
+const classes = {
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: 20,
+    marginTop: 20,
+    textAlign: "center",
+    color: "pink",
+    fontSize: "80px",
+    // fontFamily: "Roboto",
+    height: 700,
+    width: "96vw",
+    backgroundColor: "transparent",
+    // border: "1px solid white",
+  },
+};
+
+const Title = styled.h1`
+  font-size: 5em;
+  text-align: center;
+  color: #ffffff;
+`;
+
+const WordList = styled.span`
+  color: #ffff00;
+`;
 
 const Hero = () => {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 2000,
+      backSpeed: 80,
+      strings: ["Human", "Friend", "Learner", "Developer", "Snowboarder"],
+    });
+  });
   return (
     <>
-      <Box
-        component="span"
-        sx={{
-          width: 300,
-          height: 300,
-          backgroundColor: "primary.dark",
-          "&:hover": {
-            backgroundColor: "primary.main",
-            opacity: [0.9, 0.8, 0.7],
-          },
-        }}
-      >
-        This is the box
-      </Box>
+      <Grid container>
+        <Grid item xs={12}>
+          <Title>
+            <Paper style={classes.paper}>
+              Hi. I'm Chris <br />{" "}
+              <WordList ref={textRef}>
+                <br />
+              </WordList>
+            </Paper>
+          </Title>
+        </Grid>
+        <Grid item xs={12}>
+          <Title>
+            <Paper style={classes.paper}>
+              I like to build things on the internet <br />
+            </Paper>
+          </Title>
+        </Grid>
+      </Grid>
     </>
   );
 };
 
 export default Hero;
-
-// import React, { useRef, useEffect } from "react";
-
-// import { init } from "ityped";
-
-// import { Row, Col, Divider } from "antd";
-// import "antd/dist/antd.css";
-
-// import styled from "styled-components";
-
-// const HeroStyle = {
-//   // border: "1px solid white",
-//   height: "800px",
-// };
-
-// const Wrapper = styled.section`
-//   padding: 4em;
-//   background: transparent;
-// `;
-
-// const Title = styled.h1`
-//   font-size: 5em;
-//   text-align: center;
-//   color: #ffffff;
-// `;
-
-// const WordList = styled.span`
-//   color: #ffff00;
-// `;
-
-// const Hero = () => {
-//   const textRef = useRef();
-
-//   useEffect(() => {
-//     init(textRef.current, {
-//       showCursor: false,
-//       backDelay: 2000,
-//       backSpeed: 80,
-//       strings: ["Human", "Friend", "Learner", "Developer", "Snowboarder"],
-//     });
-//   });
-//   return (
-//     <>
-//       <Row align="middle">
-//         <Col style={HeroStyle} xs={2} xxl={4} xl={3}>
-//           Col
-//         </Col>
-//         <Col style={HeroStyle} xs={20} xxl={16} xl={18}>
-//           <Wrapper>
-//             <Title>
-//               Hi. I'm Chris <br />
-//               <WordList ref={textRef}>
-//                 <br />
-//               </WordList>
-//             </Title>
-//           </Wrapper>
-//         </Col>
-//         <Col style={HeroStyle} xs={2} xxl={4} xl={3}>
-//           Col
-//         </Col>
-//       </Row>
-//       <Divider />
-
-//       <Row align="middle">
-//         <Col style={HeroStyle} xs={2} xxl={4} xl={3}>
-//           Col
-//         </Col>
-//         <Col style={HeroStyle} xs={20} xxl={16} xl={18}>
-//           <Wrapper>
-//             <Title>
-//               I like to build stuff on the internet <br />
-//             </Title>
-//           </Wrapper>
-//         </Col>
-//         <Col style={HeroStyle} xs={2} xxl={4} xl={3}>
-//           Col
-//         </Col>
-//       </Row>
-//     </>
-//   );
-// };
-
-// export default Hero;
