385401aa38976dccaa42a01acee1bae5fccd8f00
cpesar <cpesar1@gmail.com>
2022-02-18 23:58:22 -0700

    added 2 more cards will display 3 cards at a time with a scroll arrow to view more projects

--- src/components/pages/ProjectTrial.jsx
+++ src/components/pages/ProjectTrial.jsx
@@ -9,35 +9,92 @@ import {
   CardContent,
   CardMedia,
   Typography,
+  // Divider,
 } from "@mui/material";
 
 function Projects() {
   return (
     <Grid container direction="row" justifyContent="center" alignItems="center">
-      <Card sx={{ maxWidth: 600, marginTop: 15 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-        </CardActions>
-      </Card>
+      <Grid>
+        <Card sx={{ maxWidth: 600, marginTop: 15 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              View the site!
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+
+      <Grid>
+        <Card sx={{ maxWidth: 600, marginTop: 15 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              View the site!
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+
+      <Grid>
+        <Card sx={{ maxWidth: 600, marginTop: 15 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              View the site!
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
     </Grid>
   );
 }
