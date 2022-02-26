e58b0b097312e45498867b182566cf06210d0e6d
cpesar <cpesar1@gmail.com>
2022-02-26 14:06:26 -0700

    merge to switch to resumepage

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,67 +1,83 @@
 import * as React from "react";
 
-import { Typography, Grid, Paper } from "@mui/material";
+import { Typography, Grid, Paper, Box } from "@mui/material";
 
 import SchoolIcon from "@mui/icons-material/School";
 import ComputerIcon from "@mui/icons-material/Computer";
 
-const classes = {
-  root: {
-    flexGrow: 1,
-  },
-};
+// const classes = {
+//   root: {
+//     flexGrow: 1,
+//   },
+// };
 
 function Resume() {
   return (
-    <div style={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper
-            sx={{
-              padding: 20,
-              marginTop: -5,
-              textAlign: "center",
-              color: "white",
-              height: 300,
-              backgroundColor: "transparent",
-            }}
-          >
-            <Typography variant="h2" color="primary">
-              Education
-            </Typography>
-            <SchoolIcon style={{ fontSize: "80px" }} />
+    // <div style={classes.root}>
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <Box
+          sx={{
+            marginTop: 10,
+            textAlign: "center",
+            color: "white",
+            backgroundColor: "transparent",
+            border: "5px solid pink",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h2" color="primary">
+            Education
+          </Typography>
+          <SchoolIcon style={{ fontSize: "80px" }} />
 
-            <div>
-              <Typography gutterBottom variant="h6">
-                Towson University
-              </Typography>
-              <span>Bachelors of Business Administration</span>
-              <Typography variant="h6">University of Utah</Typography>
-              <span>Full stack web development bootcamp</span>
-            </div>
-          </Paper>
-          <Paper
-            sx={{
-              padding: 20,
-              textAlign: "center",
-              color: "white",
-              backgroundColor: "transparent",
-            }}
-          >
-            <Typography variant="h2" color="primary">
-              Skills
+          <div>
+            <Typography gutterBottom variant="h6" color="yellow">
+              Towson University
             </Typography>
-            <ComputerIcon style={{ fontSize: "80px" }} />
+            <span>Bachelors of Business Administration</span>
 
-            <div>
-              <br /> <br /> Towson University{" "}
-              <span>Bachelors of Business Administration</span>
-              <br /> <br /> University of Utah Coding Bootcamp
-            </div>
-          </Paper>
-        </Grid>
+            <Typography gutterBottom variant="h6" color="red">
+              University of Utah
+            </Typography>
+            <span>Full stack web development bootcamp</span>
+          </div>
+        </Box>
       </Grid>
-    </div>
+      <Grid item xs={12} sm={6}>
+        <Box
+          sx={{
+            marginTop: 10,
+            textAlign: "center",
+            color: "white",
+            backgroundColor: "transparent",
+            border: "5px solid pink",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h2" color="primary">
+            Skills
+          </Typography>
+          <ComputerIcon style={{ fontSize: "80px" }} />
+
+          <div>
+            <Typography gutterBottom variant="h6" color="yellow">
+              Towson University
+            </Typography>
+            <span>Bachelors of Business Administration</span>
+
+            <Typography gutterBottom variant="h6" color="red">
+              University of Utah
+            </Typography>
+            <span>Full stack web development bootcamp</span>
+          </div>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
